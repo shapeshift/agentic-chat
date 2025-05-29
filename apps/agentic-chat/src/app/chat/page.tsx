57cc4cfbcea4b8e3ca2b64f74c367f0b6d42ec03
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-05-29 23:04:06 +0200

    feat: latest chat redirect

--- apps/agentic-chat/src/app/chat/page.tsx
+++ apps/agentic-chat/src/app/chat/page.tsx
@@ -1,7 +1,13 @@
 import { redirect } from 'next/navigation';
-import { createChat } from '../../tools/chat-store';
+import { createChat, getLatestChatId } from '../../tools/chat-store';
 
 export default async function Page() {
+  const latestChatId = await getLatestChatId();
+
+  if (latestChatId) {
+    redirect(`/chat/${latestChatId}`);
+  }
+
   const id = await createChat(); // create a new chat
   redirect(`/chat/${id}`); // redirect to chat page
 }
--- apps/agentic-chat/src/tools/chat-store.ts
+++ apps/agentic-chat/src/tools/chat-store.ts
@@ -1,16 +1,25 @@
-import { generateId, Message } from 'ai';
+import { Message } from 'ai';
 import { existsSync, mkdirSync } from 'fs';
-import { readFile, writeFile } from 'fs/promises';
+import { readdir, readFile, writeFile } from 'fs/promises';
 import path from 'path';
 
+/**
+ * Simple chat store that saves chats to the file system.
+ * This is obviously for dev purposes only, although using an actual db will just be a matter
+ * of storing/retrieving from a diff place, the shape of the data or the API of utils should
+ * not change.
+**/
+
+const CHATS_DIR = '.chats';
+
 export async function createChat(): Promise<string> {
-  const id = generateId();
+  const id = Date.now().toString()
   await writeFile(getChatFile(id), '[]');
   return id;
 }
 
 function getChatFile(id: string): string {
-  const chatDir = path.join(process.cwd(), '.chats');
+  const chatDir = path.join(process.cwd(), CHATS_DIR);
   if (!existsSync(chatDir)) mkdirSync(chatDir, { recursive: true });
   return path.join(chatDir, `${id}.json`);
 }
@@ -28,4 +37,22 @@ export async function saveChat({
 }): Promise<void> {
   const content = JSON.stringify(messages, null, 2);
   await writeFile(getChatFile(id), content);
+}
+
+export async function getLatestChatId(): Promise<string | null> {
+  const chatDir = path.join(process.cwd(), CHATS_DIR);
+  if (!existsSync(chatDir)) return null;
+
+  const files = await readdir(chatDir);
+  if (!files.length) return null;
+
+  // Gets the latest chat file based on the timestamp in the filename
+
+  const latestChat = files.sort((a, b) => {
+    const aTime = parseInt(a.split('.')[0], 10);
+    const bTime = parseInt(b.split('.')[0], 10);
+    return bTime - aTime; // Sort in descending order
+  })
+
+  return latestChat[0].split('.')[0]; // Return the ID part of the filename
 }
