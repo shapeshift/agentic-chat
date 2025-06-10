1499d7f5f6b5a75df46036b827267bdaa6cc2ae2
gomes <17035424+gomesalexandre@users.noreply.github.com>
2025-06-10 17:42:25 +0200

    feat: debug venice

--- apps/agentic-server/index.ts
+++ apps/agentic-server/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 import cors from 'cors';
 import express, { Request, Response } from 'express';
 import { mastra } from './src/mastra';
+import { inspect } from 'util';
 
 const app = express();
 app.use(express.json());
@@ -16,7 +17,14 @@ app.post('/', async (req: Request, res: Response) => {
   const agent = mastra.getAgent('shapeshiftAgent');
   const result = await agent.stream(messages, { clientTools: tools });
 
+
   result.pipeDataStreamToResponse(res);
+
+  const reqBody = (await (result.request)).body
+
+  debugger
+
+  console.log({req: inspect(reqBody, false, null, true)});
 });
 
 app.listen(8080, () => {
