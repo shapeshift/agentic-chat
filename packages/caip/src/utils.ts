3cf258e581567030759d5cd7779e6b7e2d001de3
Jibles <premiumjibles@gmail.com>
2025-09-30 10:30:11 +1000

    fix: fix deployment by pulling out caip package

---
+++ packages/caip/src/utils.ts
@@ -0,0 +1,10 @@
+import type { ChainNamespace, ChainReference } from './constants.js'
+import { VALID_CHAIN_IDS } from './constants.js'
+
+export const isValidChainPartsPair = (
+  chainNamespace: ChainNamespace,
+  chainReference: ChainReference,
+): boolean => {
+  const validChainIds = VALID_CHAIN_IDS[chainNamespace]
+  return validChainIds?.includes(chainReference) || false
+}
